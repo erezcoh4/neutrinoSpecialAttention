19a32dd203d0f972f28755683475b92fdd52b46f
Erez Cohen <erezcohen@Erezs-iMac.local>
2016-03-19 11:58:01 +0200

    only changed macro

--- nuSpecAttPackage/mac/ana_nu_n.py
+++ nuSpecAttPackage/mac/ana_nu_n.py
@@ -3,9 +3,13 @@ from ROOT import TPlots
 from ROOT import TAnalysis
 from rootpy.interactive import wait
 from ROOT import EnuNCC
+ROOT.gStyle.SetOptStat(0000)
 
-colorWeighted   = 2
+cutGen = ROOT.TCut()
 colorGen        = 1
+colorWeighted   = 2
+cutW = ROOT.TCut("XsecWeight")
+
 Nbins           = 50
 
 NModel = "CFG"
@@ -13,11 +17,10 @@ NModel = "CFG"
 ana = TPlots("/Users/erezcohen/Desktop/uboone/SpecialAttention/AnaFiles/CCinteractions"+NModel+".root" , "anaTree" , NModel )
 analysis = TAnalysis()
 
-c = ana.CreateCanvas("n momentum dist.","Divide",3,2)
+c = ana.CreateCanvas("n momentum dist.","Divide",3,3)
 
 c.cd(1)
-cut = ROOT.TCut()
-hGen = ana.H1("n.P()",cut,"HIST",Nbins,0,0.7,"n momentum dist. (" + NModel + ")","n momentum [GeV/c]","",colorGen,colorGen)
+hGen = ana.H1("n.P()",cutGen,"HIST",Nbins,0,0.7,"n momentum dist. (" + NModel + ")","n momentum [GeV/c]","",colorGen,colorGen)
 Integral = 0
 Tail = 0
 k_F = 0.25
@@ -29,10 +32,9 @@ for bin in range (1,Nbins):
 
 FracTailGen = Tail / Integral;
 print "fraction in generated tail is %f" % FracTailGen
-#analysis.NormalizeHistogram(hGen)
+analysis.NormalizeHistogram(hGen)
 
-cut = ROOT.TCut("XsecWeight")
-hWeighted = ana.H1("n.P()",cut,"HIST same",Nbins,0,0.7,"","","",colorWeighted,colorWeighted)
+hWeighted = ana.H1("n.P()",cutW,"HIST same",Nbins,0,0.7,"","","",colorWeighted,colorWeighted)
 Integral = 0
 Tail = 0
 for bin in range (1,Nbins):
@@ -43,47 +45,53 @@ for bin in range (1,Nbins):
 
 FracTailWeighted = Tail / Integral;
 print "fraction in weighted tail is %f" % FracTailWeighted
-#analysis.NormalizeHistogram(hWeighted)
+analysis.NormalizeHistogram(hWeighted)
 ana.AddLegend("n momentum dist.",hGen,"generated",hWeighted,"weighted","F");
 
 
 
 c.cd(2)
-cut = ROOT.TCut()
-ana.H2("nu.E()","XsecWeight",cut,"colz",Nbins,0,2,Nbins,0,0.7,"cross-section weighting","E#nu [GeV]","Xsec weight (a.u.)")
+ana.H2("nu.E()","XsecWeight",cutGen,"colz",Nbins,0,2,Nbins,0,0.7,"cross-section weighting","E#nu [GeV]","Xsec weight (a.u.)")
 nuNCC = EnuNCC()
 nuNCC.ImpXsecGraph("/Users/erezcohen/Desktop/uboone/SpecialAttention/Data/Xsec.dat" , 70 , False )    # neutrino - n cross section by A. Schukraft
 g = nuNCC.GetXsecEGraph();
 g.Draw("same c")
 
+
 c.cd(3)
-cut = ROOT.TCut()
-ana.H1("XsecWeight",cut,"HIST",Nbins,0,0.7,"cross-section weighting","Xsec weight (a.u.)","",4,46)
+ana.H1("XsecWeight",cutGen,"HIST",Nbins,0,0.7,"cross-section weighting","Xsec weight (a.u.)","",4,46)
 
 
 
 c.cd(4)
-cut = ROOT.TCut()
-hEv = ana.H1("nu.E()",cut,"HIST",Nbins,0,2,"#nu energy (lab)","E#nu [GeV]","",colorGen,colorGen)
-cut = ROOT.TCut("XsecWeight")
-hEvW = ana.H1("nu.E()",cut,"HIST same",Nbins,0,2,"","E#nu [GeV]","",colorWeighted,colorWeighted)
+hEv = ana.H1("nu.E()",cutGen,"HIST",Nbins,0,2,"#nu energy (lab)","E#nu [GeV]","",colorGen,colorGen)
+analysis.NormalizeHistogram(hEv)
+hEvW = ana.H1("nu.E()",cutW,"HIST same",Nbins,0,2,"","E#nu [GeV]","",colorWeighted,colorWeighted)
+analysis.NormalizeHistogram(hEvW)
 ana.AddLegend("Lab Frame E#nu",hEv,"generated",hEvW,"weighted","F");
 
 
 c.cd(5)
-cut = ROOT.TCut()
-hEv_INnRF = ana.H1("nu_INnRF.E()",cut,"HIST",Nbins,0,2,"#nu energy in n RestFrame","E#nu [GeV]","",colorGen,colorGen)
-cut = ROOT.TCut("XsecWeight")
-hEvW_INnRF = ana.H1("nu_INnRF.E()",cut,"HIST same",Nbins,0,2,"","E#nu [GeV]","",colorWeighted,colorWeighted)
+hEv_INnRF = ana.H1("nu_INnRF.E()",cutGen,"HIST",Nbins,0,2,"#nu energy in n RestFrame","E#nu [GeV]","",colorGen,colorGen)
+analysis.NormalizeHistogram(hEv_INnRF)
+hEvW_INnRF = ana.H1("nu_INnRF.E()",cutW,"HIST same",Nbins,0,2,"","E#nu [GeV]","",colorWeighted,colorWeighted)
+analysis.NormalizeHistogram(hEvW_INnRF)
 ana.AddLegend("Rest Frame E#nu",hEv_INnRF,"generated",hEvW_INnRF,"weighted","F");
 
 
 
 
 c.cd(6)
-cut = ROOT.TCut()
-ana.H1("nu_INnRF.E() - nu.E()",cut,"HIST",Nbins,-0.5,0.5,"boosting #nu","E#nu (Resf Frame) - E#nu (lab) [GeV]","",1,1)
-#ana.H1("nu_INnRF.Pz() - nu.Pz()",cut,"HIST",Nbins,-0.5,0.5,"boosting #nu","p(#nu) (Resf Frame) - p(#nu) (lab) [GeV/c]","",1,1)
+ana.H1("nu_INnRF.E() - nu.E()",cutGen,"HIST",Nbins,-0.5,0.5,"boosting #nu","E#nu (Resf Frame) - E#nu (lab) [GeV]","",1,1)
+
+
+c.cd(7)
+ana.H2("n.Pz()","nu_INnRF.Pz() - nu.Pz()",cutGen,"colz",Nbins,-1,1,Nbins,-1,1,"cross-section weighting","p(n)^{z} [GeV/c]","#Delta p(#nu)^{z} [GeV/c]")
+
+
+c.cd(8)
+ana.H2("n.P()","XsecWeight",cutGen,"colz",Nbins,0,1,Nbins,0,0.7,"cross-section weighting","n momentum [GeV/c]","Xsec weight (a.u.)")
+
 
 
 wait()
